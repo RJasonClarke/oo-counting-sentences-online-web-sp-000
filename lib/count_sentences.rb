19e936752eb28755a597b3c74da009f711137d9e
Robert Clarke <r.jason.clarke@gmail.com>
2020-10-12 14:43:51 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -21,7 +21,7 @@ class String
   def count_sentences
     count_sentences = "one. two. three?"
     count_sentences.split(/\.|\?|!/).length
-    if .split(/\.|\?|!/).length == 0
+    if count_sentences.split(/\.|\?|!/).length == 0
       return "0"
   end
 end
